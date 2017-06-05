7f92834f0f808b26f285e951c4ca1f13efdc6b91
Werner Huber <werner.huber@sixt.com>
2017-06-04 19:59:16 -0700

    initial commit

---
+++ MingaApp/MingaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MingaApp
+//
+//  Created by Werner Huber on 04.06.17.
+//  Copyright © 2017 MingaApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
