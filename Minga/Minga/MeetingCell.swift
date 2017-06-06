eeb594b091c4649ae6bea3610d7b982d198dd135
Manu Rink <manuela.schrenk@gmail.com>
2017-06-05 20:30:51 -0700

    implement first draft of meetinglistcontroller with its meeting cells

---
+++ Minga/Minga/MeetingCell.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingCell.swift
+//  Minga
+//
+//  Created by Manu Rink on 05.06.17.
+//  Copyright © 2017 MingaApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MeetingCell : UITableViewCell {
+    
+    override func awakeFromNib() {
+        
+    }
+}
--- Minga/Minga/MeetingListController.swift
+++ Minga/Minga/MeetingListController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MeetingListController : UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "meetingcell")
+        return cell!
     }
 
 
